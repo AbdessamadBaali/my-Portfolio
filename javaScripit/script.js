a8a420f663ee5878ef1681a854e341a27b442b6b
Abdessamad BAALI <91624658+AbdessamadBaali@users.noreply.github.com>
2021-12-27 00:13:31 +0100

    IT'S ME

--- javaScripit/script.js
+++ javaScripit/script.js
@@ -1,12 +1,8 @@
 // read more btn script
 let changeReadMore = document.querySelector("#read-more-btn")
-
-
 function readMoreScript() {
-
     if (changeReadMore.textContent === "Read More...") {
-         changeReadMore.textContent = "Read Less..."
-        
+         changeReadMore.textContent = "Read Less..."     
     } else if (changeReadMore.textContent === "Read Less...") {
         changeReadMore.textContent = "Read More..."
     } else {
