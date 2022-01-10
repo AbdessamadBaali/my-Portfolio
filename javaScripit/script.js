a144c525aa57f14025f458e2a459a14c74733a51
Abdessamad BAALI <91624658+AbdessamadBaali@users.noreply.github.com>
2022-01-10 08:56:20 +0100

    my portfolio

--- javaScripit/script.js
+++ javaScripit/script.js
@@ -9,27 +9,25 @@ function readMoreScript() {
         changeReadMore.textContent = "Read Less..."
     } 
 }
-
 // forme
-const sendFeedback = (emailjs, serviceID, templateId, variables, userId) => {
-    emailjs.send(serviceID, templateId, variables, userId).then(res => {
-        alert('Email successfully sent!')
-    })
-        .catch(err => alert.error('There has been an error.  Here some thoughts on the error that occured:', err))
-}
-$("form-contact").submit((e) => {
-    e.preventDefault()
-    const name = e.target[0].value
-    const email = e.target[1].value
-    const message = e.target[2].value
+// const sendFeedback = (emailjs, serviceID, templateId, variables, userId) => {
+//     emailjs.send(serviceID, templateId, variables, userId).then(res => {
+//         alert('Email successfully sent!')
+//     })
+//         .catch(err => alert.error('There has been an error.  Here some thoughts on the error that occured:', err))
+// }
+// $("form-contact").submit((e) => {
+//     e.preventDefault()
+//     const name = e.target[0].value
+//     const email = e.target[1].value
+//     const message = e.target[2].value
 
-    alert(`Thank you for your message from ${email}`);
-    const templateId = 'template_ga13ufz';
-    const serviceID = 'service_6yqhq29';
-    const userId = "user_6uDwTYCySYHyDYjqRGDnu"
-    const data = { from_name: name, message, from_email: email, to_name: 'Abdessamad' }
-    import('emailjs-com').then(emailjs => sendFeedback(emailjs, serviceID, templateId, data, userId))
+//     alert(`Thank you for your message from ${email}`);
+//     const templateId = 'template_ga13ufz';
+//     const serviceID = 'service_6yqhq29';
+//     const userId = "user_6uDwTYCySYHyDYjqRGDnu"
+//     const data = { from_name: name, message, from_email: email, to_name: 'Abdessamad' }
+//     import('emailjs-com').then(emailjs => sendFeedback(emailjs, serviceID, templateId, data, userId))
     
-})
+// })
 
- 
