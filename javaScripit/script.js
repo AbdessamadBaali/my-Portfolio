c3df06ec7ba25fd9d09bf11aebca3a203aa78120
AbdessamadBaali <baaliabdessamad3@gmail.com>
2023-02-11 23:44:37 +0100

    chnage content and design

--- javaScripit/script.js
+++ javaScripit/script.js
@@ -17,40 +17,48 @@ $(document).ready( () => {
         }
     })
 
-
-
-		
 });
 
 
+function sendMail() {
+    const form = document.getElementById("form-contact");
 
-
-
-// email generatore
-// import $ from 'jquery'
-// const sendFeedback = (emailjs, serviceID, templateId, variables, userId) => {
-//     emailjs.send(
-//         serviceID, templateId,
-//         variables, userId
-//     ).then(res => {
-//         alert('Email successfully sent!')
-//     })
-//         .catch(err => alert.error('There has been an error.  Here some thoughts on the error that occured:', err))
-// }
-
-// $("#form-contact").submit((e) => {
-//     e.preventDefault()
-
-//     const name = e.target[0].value
-//     const email = e.target[1].value
-//     const phone = e.target[2].value
-//     const message = e.target[3].value
-
-//     alert(`Thank you for your message from ${email}`);
-//     const templateId = 'template_ga13ufz';
-//     const serviceID = 'my-email';
-//     const userId = "user_6uDwTYCySYHyDYjqRGDnu"
-//     const data = { from_name: name, message,from_email: phone, from_email: email, to_name: 'Abdessamad' }
-//     import('emailjs-com').then(emailjs => sendFeedback(emailjs, serviceID, templateId, data, userId))
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
     
-// })
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("mail").value;
+      const phone = document.getElementById("phone").value;
+      const message = document.getElementById("msg").value;
+    
+      const data = {
+        apikey: "01196A8034249E557A7B52C81CF03647354AE89080699399A1EBF9680808A2E1AA266C2D5F6CA6975945A01A13A5C95C",
+        subject: "Contact Me",
+        from: email,
+        to: "abdessamadbaali3@gmail.com",
+        bodyHtml: `<p>Name: ${name}</p><p>Email: ${email}</p><p>phone: ${phone}</p><p>Message: ${message}</p>`
+      };
+    
+      // Send the data to Elastic Email API
+      fetch("https://api.elasticemail.com/v2/email/send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => {
+          if (response.ok) {
+            // show success message
+            return;
+          }
+          throw new Error("Network response was not ok");
+        })
+        .catch(error => {
+          // show error message
+          console.error("There was a problem with the fetch operation:", error);
+        });
+    });
+    
+}
+
